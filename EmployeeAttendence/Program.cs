afcf0c78682dee5d4cbbb3890656a7c73ccf629c
yamini <76487456+vundelayamini@users.noreply.github.com>
2021-03-03 21:46:22 +0530

    Update Program.cs

--- EmployeeAttendence/Program.cs
+++ EmployeeAttendence/Program.cs
@@ -8,9 +8,6 @@ namespace EmployeeAttendence
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
-            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
