5ce1c3c260f57fc344759b96a02fd1a401369d63
vundelayamini <vundelayamini@gmail.com>
2021-03-03 21:32:27 +0530

    [yamini] ADD employee daily wage

--- EmployeeAttendence/Program.cs
+++ EmployeeAttendence/Program.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿
+using System;
 
-namespace EmployeeAttendence
+namespace EmployeeDailyWage
 {
     class Program
     {
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
+            {
+                empHrs = 8;
             else
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage :" + empWage);
+
             Console.ReadKey();
         }
     }
