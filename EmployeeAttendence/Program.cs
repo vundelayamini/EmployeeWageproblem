126b305301650a4e61915d3e287bd5ead92bfd6f
yamini <76487456+vundelayamini@users.noreply.github.com>
2021-03-03 21:53:28 +0530

    Update Program.cs

--- EmployeeAttendence/Program.cs
+++ EmployeeAttendence/Program.cs
@@ -8,15 +8,20 @@ namespace EmployeeAttendence
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs=8;
+                
             else
-                ConConsole.WriteLine("Employee is absent");
-                Console.ReadKey();
-            }    
+                empHrs=0;
+            }   
+             empWage= empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage : " + empWage )
         }
     }
 }
