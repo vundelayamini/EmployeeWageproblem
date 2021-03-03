522d48125b44c4473c47f2f85b81cc49b6211868
vundelayamini <vundelayamini@gmail.com>
2021-03-03 22:07:21 +0530

    [yamini] ADD : calculate employee wage

--- EmployeeAttendence/Program.cs
+++ EmployeeAttendence/Program.cs
@@ -1,18 +1,19 @@
 ﻿
 using System;
 
-namespace EmployeeDailyWage
+namespace CalculateWages
 {
     class Program
     {
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int Is_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
