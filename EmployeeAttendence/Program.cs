353fd1243d7b33a6d6d3c2f3f12e37c0e9eb44f1
vundelayamini <vundelayamini@gmail.com>
2021-03-04 12:47:30 +0530

    [yamini]Add: wages for month

--- EmployeeAttendence/Program.cs
+++ EmployeeAttendence/Program.cs
@@ -5,29 +5,38 @@ namespace Employee_Details
 {
     class Program
     {
+         public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 50;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            //int IS_FULL_TIME = 1;
-            //int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+          //variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            //computation
+            for (int temp = 0; temp < NUM_OF_WORKING_DAYS; temp++)
             {
-                case 2:
-                    empHrs = 4;
-                    break;
-                case 1:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage :" + empWage);
+            Console.WriteLine("Total Monthly Emp Wage : " + totalEmpWage);
+
         }
     }
 }
