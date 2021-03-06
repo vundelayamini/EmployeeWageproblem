3b4b17d1a7540f8e412c612f06e99dde92005855
vundelayamini <vundelayamini@gmail.com>
2021-03-06 15:57:07 +0530

    [yamini]Add: manage Employeen Wages of Multiple Companies

---
+++ EmpwageMultipleCom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC10_ManageEmpwageMultipleCom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
