64b34cc708af4952d3021efae34f4596a0456728
vundelayamini <vundelayamini@gmail.com>
2021-03-06 12:57:46 +0530

    [yamini]Add: calculate employee Wage

---
+++ EmployeeWageProblem/UC7-ComputeEmployeeWage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageproblem
+{
+    class UC7_ComputeEmployeeWage
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+        public static int calculateEmployee()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUMBER_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            return totalEmpWage;
+        }
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine($"Total Employee Wage is : {calculateEmployee()}");
+        }
+}
