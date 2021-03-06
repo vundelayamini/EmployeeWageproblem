3f3800054cf0358becb66fa64094bb83e45c3e6f
yamini <76487456+vundelayamini@users.noreply.github.com>
2021-03-06 15:29:08 +0530

    Update Program.cs

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,8 +6,13 @@ namespace EmployeeWageProblem
     {
         static void Main(String[] args)
         {
-
-            Console.WriteLine($"Total Employee Wage is : {calculateEmployee()}");
+            EmpWage dMart = new EmpWage("DMart", 20, 2, 10);
+            EmpWage reliance = new EmpWage("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            reliance.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            Console.WriteLine(reliance.toString());
+           
         }
     }
 }
