69fe1101d555b748241033cd97988ba2d636e64e
vundelayamini <vundelayamini@gmail.com>
2021-03-06 14:37:35 +0530

    [yamini]Add: calculate employee wage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EmployeeWage
+namespace EmployeeWageproblem
 {
-    class EmpWage
+    class UC7_ComputeEmployeeWage
     {
-        public const int IS_FULL_TIME = 2;
-        public const int IS_PART_TIME = 1;
-         public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+        public static int calculateEmployee()
         {
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays <= numOfWorkingDays)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUMBER_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -30,8 +34,12 @@ namespace EmployeeWage
                 }
                 totalEmpHrs += empHrs;
             }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             return totalEmpWage;
         }
-    }
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine($"Total Employee Wage is : {calculateEmployee()}");
+        }
 }
