a448d0c505ad8d03c972128a91421ea4d60cb746
yamini <76487456+vundelayamini@users.noreply.github.com>
2021-03-11 20:51:18 +0530

    Update Program.cs

--- FinalSolution/Program.cs
+++ FinalSolution/Program.cs
@@ -6,7 +6,13 @@ namespace FinalSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+                empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+                empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+                empWageBuilder.computeEmpWage();
+                //Console.WriteLine("Total Wage for DMart company: "+empWageBuilder.getTotalWage("DMart"));
+                Console.ReadKey();
+            }
         }
     }
 }
